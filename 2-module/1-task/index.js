676f53cf43319220ddefe2f23968f6996ba20f59
Vitalii Aleksandrov <vitalaa547@gmail.com>
2024-02-28 16:01:40 +0400

    Модуль 2 задание1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,31 @@
 function sumSalary(salaries) {
   // ваш код...
-}
+  
+    let sum = 0;
+  
+    for (let key in salaries) {
+      if (typeof salaries[key] === 'number' && !isNaN(salaries[key]) && isFinite(salaries[key])) {
+        sum += salaries[key];
+      }
+    }
+  
+    return sum;
+  }
+  
+  let salaries = {
+    John: 1000,
+    Ann: 1600,
+    Pete: 1300,
+    month: 'December',
+    currency: 'USD',
+    isPayed: false,
+    specialValue: NaN
+  };
+  
+  console.log(sumSalary(salaries)); // Output: 3900
+   
+ 
+  
+  
+  
+
