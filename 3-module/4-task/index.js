2c9831195dd93722926ed266e6e5841bd50e3e42
Vitalii Aleksandrov <vitalaa547@gmail.com>
2024-03-04 16:18:24 +0400

    Модуль 3 задание3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,14 @@
 function showSalary(users, age) {
   // ваш код...
+  
+  return users
+    .filter(item => item.age <= age)
+    .map(item => `${item.name}, ${item.balance}`)
+    .join('\n');
 }
+  
+  
+ 
+  
+ 
+  
