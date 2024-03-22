4587d62a49dd00cd67bcfefff4468b696a3a9167
Vitalii Aleksandrov <vitalaa547@gmail.com>
2024-03-22 13:36:31 +0400

    Модуль 7 задача 4(проект)

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,21 +2,40 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
-    this.modalElement = null;
-    this.modalTitleElement = null;
-    this.modalBodyElement = null;
-    this.closeButtonElement = null;
-    this.isOpen = false;
-    this.tempTitle = null;
-    this.tempBody = null;
-    this.overlayElement = null;
+    this.render();
+    // this.modalElement = null;
+    // this.modalTitleElement = null;
+    // this.modalBodyElement = null;
+    // this.closeButtonElement = null;
+    // this.isOpen = false;
+    // this.tempTitle = null;
+    // this.tempBody = null;
+    // this.overlayElement = null;
 
-    this.open = this.open.bind(this);
-    this.close = this.close.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
+    // this.open = this.open.bind(this);
+    // this.close = this.close.bind(this);
+    // this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
-  createOverlay() {
+
+  render() {
+     this.modal = createElement(`
+       <div class="modal">
+         <div class="modal__overlay"></div>
+         <div class="modal__inner">
+           <div class="modal__header">
+             <button type="button" class="modal__close">
+               <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+             </button>
+             <h3 class="modal__title"></h3>
+           </div>
+           <div class="modal__body"></div>
+         </div>
+       </div>
+     `);
+  }
+  // Зачем так сложно?
+  /*createOverlay() {
     this.overlayElement = document.createElement('div');
     this.overlayElement.classList.add('modal-overlay');
     document.body.appendChild(this.overlayElement);
@@ -52,66 +71,74 @@ export default class Modal {
     this.modalElement.appendChild(this.closeButtonElement);
 
     document.body.appendChild(this.modalElement);
-  }
+  }*/
 
   open() {
-    if (!this.isOpen) {
+    document.body.append(this.modal);
+    document.body.classList.add('is-modal-open');
+    document.addEventListener('keydown', this.handleKeyDown);
+    this.modal.querySelector('button.modal__close').addEventListener('click', this.close);
+    /*if (!this.isOpen) {
       if (!this.overlayElement) {
         this.createOverlay();
       }
       if (!this.modalElement) {
-        this.createModal();
+        // this.createModal();
       }
       document.body.appendChild(this.modal);
     
       document.body.classList.add('is-modal-open');
       window.addEventListener('keydown', this.handleKeyDown);
       this.isOpen = true;
-    }
+    }*/
   }
 
   setTitle(title) {
-    if (this.isOpen && this.modalTitleElement) {
-      this.modalTitleElement.textContent = title;
-    } else {
-      this.tempTitle = title;
-    }
+    this.modal.querySelector('.modal__title').textContent = title;
+    // if (this.isOpen && this.modalTitleElement) {
+    //   this.modalTitleElement.textContent = title;
+    // } else {
+    //   this.tempTitle = title;
+    // }
   }
 
   setBody(node) {
-    if (this.isOpen && this.modalBodyElement) {
+    this.modal.querySelector('.modal__body').innerHTML = ''; // очищаем старое body
+    this.modal.querySelector('.modal__body').append(node);
+    // if (this.isOpen && this.modalBodyElement) {
      
-      this.modalBodyElement.appendChild(node);
-    } else {
-      this.tempBody = node;
-    }
+    //   this.modalBodyElement.appendChild(node);
+    // } else {
+    //   this.tempBody = node;
+    // }
   }
 
-  close() {
-    if (this.isOpen) {
+  close = () => {
+    // if (this.isOpen) {
       document.body.classList.remove('is-modal-open');
-      window.removeEventListener('keydown', this.handleKeyDown);
-      this.isOpen = false;
-      if (this.modalElement) {
-        this.modalElement.remove();
+      document.removeEventListener('keydown', this.handleKeyDown);
+      // this.isOpen = false;
+      if (this.modal) {
+        this.modal.remove();
       }
       if (this.overlayElement) {
         this.overlayElement.remove();
         this.overlayElement = null; 
       }
-    }
+    // }
   }
 
-  handleKeyDown(event) {
+  handleKeyDown = (event) => {
     if (event.code === 'Escape') {
+      event.preventDefault();
       this.close();
     }
   }
 }
 
-let modal = new Modal();
-modal.setTitle('Заголовок модального окна');
-let modalBody = document.createElement('div');
-modalBody.innerHTML = '<b>Тут содержится тело модального окна</b>';
-modal.setBody(modalBody);
-modal.open();
+// let modal = new Modal();
+// modal.setTitle('Заголовок модального окна');
+// let modalBody = document.createElement('div');
+// modalBody.innerHTML = '<b>Тут содержится тело модального окна</b>';
+// modal.setBody(modalBody);
+// modal.open();
--- 7-module/4-task/index.js
+++ 7-module/4-task/index.js
@@ -1,4 +1,87 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+   
+    this.steps = steps;
+    this.value = value;
+    this.render();
+    this.addClickEvent();
+    this.addDragEvent();
   }
+  render() {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('slider');
+    this.elem.innerHTML =
+    `<div class="slider__thumb" style="left: ${this.value / (this.steps - 1) * 100}%;">         <span class="slider__value">${this.value}</span>       </div>       <div class="slider__progress" style="width: ${this.value / (this.steps - 1) * 100}%;"></div>       <div class="slider__steps">         ${'<span></span>'.repeat(this.steps)}       </div>
+    ;`
+    this.stepsContainer = this.elem.querySelector('.slider__steps');
+    
+this.stepsContainer.children[this.value].classList.add('slider__step-active');
 }
+
+addClickEvent() {
+  this.elem.addEventListener('click', (event) => {
+  this.updateSlider(event);
+  });
+  }
+  
+  addDragEvent() {
+  let thumb = this.elem.querySelector('.slider__thumb');
+  let progress = this.elem.querySelector('.slider__progress');
+  thumb.ondragstart = () => false;
+
+thumb.addEventListener('pointerdown', (event) => {
+  this.elem.classList.add('slider_dragging');
+
+  document.addEventListener('pointermove', this.onPointerMove);
+  document.addEventListener('pointerup', (event) => {
+    document.removeEventListener('pointermove', this.onPointerMove);
+    this.updateSlider(event);
+    this.elem.classList.remove('slider_dragging');
+  }, { once: true });
+});
+}
+onPointerMove = (event) => {
+  let left = event.clientX - this.elem.getBoundingClientRect().left;
+  let leftRelative = left / this.elem.offsetWidth;
+  let segments = this.steps - 1;
+  let approximateValue = leftRelative * segments;
+  let value = Math.round(approximateValue);
+  let valuePercents = (value / segments) * 100;
+  this.elem.querySelector('.slider__thumb').style.left = `${valuePercents}%`;
+this.elem.querySelector('.slider__progress').style.width = `${valuePercents}%`;
+
+this.stepsContainer.querySelector('.slider__step-active').classList.remove('slider__step-active');
+this.stepsContainer.children[value].classList.add('slider__step-active');
+
+this.value = value;
+this.elem.querySelector('.slider__value').textContent = value;
+
+let customEvent = new CustomEvent('slider-change', {
+  detail: value,
+  bubbles: true
+});
+this.elem.dispatchEvent(customEvent);
+}
+updateSlider(event) {
+  const left = event.clientX - this.elem.getBoundingClientRect().left;
+  const leftRelative = left / this.elem.offsetWidth;
+  let segments = this.steps - 1;
+  let approximateValue = leftRelative * segments;
+  let value = Math.round(approximateValue);
+  let valuePercents = (value / segments) * 100;
+  this.elem.querySelector('.slider__thumb').style.left = `${valuePercents}%`;
+this.elem.querySelector('.slider__progress').style.width = `${valuePercents}%`;
+
+this.stepsContainer.querySelector('.slider__step-active').classList.remove('slider__step-active');
+this.stepsContainer.children[value].classList.add('slider__step-active');
+
+this.value = value;
+this.elem.querySelector('.slider__value').textContent = value;
+
+let customEvent = new CustomEvent('slider-change', {
+  detail: value,
+  bubbles: true
+});
+this.elem.dispatchEvent(customEvent);
+}
+}    
