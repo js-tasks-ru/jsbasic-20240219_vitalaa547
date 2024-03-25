8b1c48017dc08545a1a50046a6c09ffc356a6c5f
Vitalii Aleksandrov <vitalaa547@gmail.com>
2024-03-25 13:31:17 +0400

    Модуль 8 задача 1(проект)

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -15,30 +15,68 @@ export default class CartIcon {
     if (!cart.isEmpty()) {
       this.elem.classList.add('cart-icon_visible');
 
-      this.elem.innerHTML = `
-        <div class="cart-icon__inner">
-          <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
-        </div>`;
+  this.elem.innerHTML = `
+    <div class="cart-icon__inner">
+      <span class="cart-icon__count">${cart.getTotalCount()}</span>
+      <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+    </div>`;
 
-      this.updatePosition();
+  this.updatePosition();
+  
+  this.elem.classList.add('shake');
+  this.elem.addEventListener('transitionend', () => {
+    this.elem.classList.remove('shake');
+  }, {once: true});
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
-    } else {
-      this.elem.classList.remove('cart-icon_visible');
-    }
+  } else {
+    this.elem.classList.remove('cart-icon_visible');
   }
+}
 
-  addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
-  }
+addEventListeners() {
+  document.addEventListener('scroll', () => this.updatePosition());
+  window.addEventListener('resize', () => this.updatePosition());
+}
 
-  updatePosition() {
-    // ваш код ...
+updatePosition() {
+  let isMobile = document.documentElement.clientWidth <= 767;
+  let container = document.querySelector('.container');
+  
+  if (isMobile || !container) {
+    Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+    });
+    return;
   }
+  
+  let cartIconRect = this.elem.getBoundingClientRect();
+  let containerRect = container.getBoundingClientRect();
+  
+  let initialTopCoord = cartIconRect.top + window.pageYOffset;
+  
+  if (window.pageYOffset > initialTopCoord || containerRect.right + 20 < cartIconRect.right) {
+    let leftIndent = Math.min(
+        containerRect.right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+    ) + 'px';
+  
+    Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1000,
+        right: '10px',
+        left: leftIndent
+    });
+  } else {
+    Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+    });
+  }
+}
 }
--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,58 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
-  }
-}
+    
+      
+        this.products = products;
+        this.filteredProducts = products;
+        
+        this.render();
+      }
+    
+      render() {
+        this.elem = document.createElement('div');
+        this.elem.classList.add('products-grid');
+        this.elem.innerHTML = `
+          <div class="products-grid__inner">
+            <!--ВОТ ТУТ БУДУТ КАРТОЧКИ ТОВАРОВ-->
+          </div>
+        `;
+        
+        this.products.forEach(product => {
+          const productCard = new ProductCard(product);
+          this.elem.querySelector('.products-grid__inner').appendChild(productCard.elem);
+        });
+      }
+    
+      updateFilter(filters) {
+        this.filteredProducts = this.products.filter(product => {
+          if (filters.noNuts && product.nuts === true) {
+            return false;
+          }
+          
+          if (filters.vegetarianOnly && product.vegetarian !== true) {
+            return false;
+          }
+          
+          if (filters.maxSpiciness !== undefined && filters.maxSpiciness < product.spiciness) {
+            return false;
+          }
+          
+          if (filters.category !== undefined && filters.category !== '' && filters.category !== product.category) {
+            return false;
+          }
+          
+          return true;
+        });
+    
+        this.elem.querySelector('.products-grid__inner').innerHTML = '';
+        this.filteredProducts.forEach(product => {
+          const productCard = new ProductCard(product);
+          this.elem.querySelector('.products-grid__inner').appendChild(productCard.elem);
+        });
+      }
+    }
+    
+    let productGrid = new ProductGrid(products);
+    
+  
