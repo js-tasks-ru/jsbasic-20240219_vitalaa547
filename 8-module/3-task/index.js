fcc0e0616c8728895f3ccf376fe9645f288176e3
Vitalii Aleksandrov <vitalaa547@gmail.com>
2024-03-28 14:12:57 +0400

    Модуль 8 задача 3(проект)

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,44 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) {
+      return;  // ваш код
+  } let existingItem = this.cartItems.find(item => item.product.id === product.id);
+
+  if (existingItem) {
+    existingItem.count++;
+  } else {
+    this.cartItems.push({ product: product, count: 1 });
   }
 
+  this.onProductUpdate(existingItem || { product, count: 1 });
+}
+
+
   updateProductCount(productId, amount) {
-    // ваш код
+    let itemToUpdate = this.cartItems.find(item => item.product.id === productId);
+
+    if (itemToUpdate) {
+      itemToUpdate.count += amount;
+
+      if (itemToUpdate.count <= 0) {
+        this.cartItems = this.cartItems.filter(item => item.product.id !== productId);
+      }
+
+      this.onProductUpdate(itemToUpdate);
+    } // ваш код
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;// ваш код
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.count, 0);// ваш код
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((totalPrice, item) => totalPrice + (item.product.price * item.count), 0);// ваш код
   }
 
   onProductUpdate(cartItem) {
